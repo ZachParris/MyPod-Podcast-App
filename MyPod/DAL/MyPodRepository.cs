e31edcff62d1be14d1e75557778ac5c8c5764e95
ZachParris <zacharyjparris@gmail.com>
2016-12-10 11:53:03 -0600

    added dal, erd, wireframes

---
+++ MyPod/DAL/MyPodRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyPod.DAL
+{
+    public class MyPodRepository
+    {
+        public MyPodContext Context { get; set; }
+        public MyPodRepository(MyPodContext _context)
+        {
+            Context = _context;
+        }
+
+        public MyPodRepository()
+        {
+            Context = new MyPodContext();
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -18,9 +18,5 @@ Still under construction!
 A Podcast community app designed for users to find, play, share, and discuss their favorite podcasts. 
 Utilizing ASP.NET MVC (C#) using AngularJS, Bootstrap
 
-##Stretch Goals
-
-Lots more features in the works
-
 ### Contributors:
 [Zach Parris](https://github.com/ZachParris)
